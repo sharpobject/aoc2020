24f760ee4a04dac75c394cc0d4ce1ca6691d0156
Robert Burke <sharpobject@gmail.com>
2020-12-13 14:43:25 +0900

    simplify

--- 13.lua
+++ 13.lua
@@ -9,42 +9,36 @@ local ls = io.read("*l"):split(",")
 local xs = {}
 local n = 0
 for i=1,#ls do
+  n = n + 1
   if ls[i] ~= "x" then
-    n = n + 1
     xs[n] = tonumber(ls[i])
   end
 end
-
-local best_bus = 0
-local best_wait_time = 1e99
-for i=1,n do
-  local this_wait_time = (xs[i] - (st % xs[i])) % xs[i]
-  if this_wait_time < best_wait_time then
-    best_wait_time = this_wait_time
-    best_bus = xs[i]
-  end
-end
-
-print(best_bus*best_wait_time)
 
 local function gcd(a,b)
   if b == 0 then return a end
   return gcd(b, a%b)
 end
 local function lcm(a,b)
-  return a*b/gcd(a,b)
+  return a*(b/gcd(a,b))
 end
 
+local best_bus = 0
+local best_wait_time = 1e99
 local ts = 0
 local step = 1
-for i=1,#ls do
-  if ls[i] ~= "x" then
-    local bus = tonumber(ls[i])
-    local modulus = (bus*1000-(i-1)) % bus
-    while ts % bus ~= modulus do
-      ts = ts + step
-    end
-    step = lcm(step, bus)
+for i,bus in pairs(xs) do
+  local this_wait_time = (bus - (st % bus)) % bus
+  if this_wait_time < best_wait_time then
+    best_wait_time = this_wait_time
+    best_bus = bus
   end
+  local modulus = (bus*1000-(i-1)) % bus
+  while ts % bus ~= modulus do
+    ts = ts + step
+  end
+  step = lcm(step, bus)
 end
-print(string.format("%d",ts))
+
+print(best_bus*best_wait_time)
+print(("%d"):format(ts))
