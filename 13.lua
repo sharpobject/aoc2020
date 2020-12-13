da98f33f1d4487d3e9252a61d7c8643c97e7ec96
Robert Burke <sharpobject@gmail.com>
2020-12-13 14:24:18 +0900

    still don't need bigint

--- 13.lua
+++ 13.lua
@@ -1,6 +1,5 @@
 require"stridx"
 require"util"
-local bigint = require"bigint"
 
 local xs = {}
 local n = 0
@@ -29,23 +28,23 @@ end
 print(best_bus*best_wait_time)
 
 local function gcd(a,b)
-  if tostring(b) == "0" then return a end
+  if b == 0 then return a end
   return gcd(b, a%b)
 end
 local function lcm(a,b)
   return a*b/gcd(a,b)
 end
 
-local ts = bigint:new(0)
-local step = bigint:new(1)
+local ts = 0
+local step = 1
 for i=1,#ls do
   if ls[i] ~= "x" then
-    local bus = bigint:new(ls[i])
+    local bus = tonumber(ls[i])
     local modulus = (bus*1000-(i-1)) % bus
-    while tostring(ts % bus) ~= tostring(modulus) do
+    while ts % bus ~= modulus do
       ts = ts + step
     end
     step = lcm(step, bus)
   end
 end
-print(ts)
+print(string.format("%d",ts))
